a820bcf6792fb2ad4515a8ebbd501dfcb2ae2ee9
FarrukhShomurodov <uamerike@gmail.com>
2024-10-27 21:08:06 +0500

    change time zone for test

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command('app:update-currency-rates')->daily();
+        $schedule->command('app:update-currency-rates')->dailyAt('19:30');
     }
 
     /**
